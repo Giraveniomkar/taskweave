5ff80f2024610e9e33c11c0833865f48ebde296d
Giraveni Omkar <omkaryadav2208@gmail.com>
2026-04-06 12:48:57 +0530

    remove extra router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,10 @@ import './index.css'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// 👉 ADD THIS
-import { HashRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>   {/* 👈 ADD THIS */}
       <App />
-    </HashRouter>
   </StrictMode>,
 )
